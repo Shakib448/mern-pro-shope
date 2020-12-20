68750309d2e5d0e71d9a7118ae8aed47bd51e804
Shakib448 <shakiba448@gmail.com>
2020-12-20 20:17:48 +0600

    MongoDB added

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import products from "./data/product.js";
+import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 dotenv.config();
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is running...");
