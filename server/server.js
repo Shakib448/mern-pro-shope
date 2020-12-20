66e8445696c4c6432b5195196db302918cc5fde9
Shakib448 <shakiba448@gmail.com>
2020-12-20 09:49:46 +0600

    Backhend added

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+const products = require("./data/product");
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(PORT, console.log(`Server is running on http://localhost:${PORT}`));
