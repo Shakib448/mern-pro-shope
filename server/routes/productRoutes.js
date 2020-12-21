93025eb6ee05f855e976c2ca2dcb4e4bd7b44ccf
Shakib448 <shakiba448@gmail.com>
2020-12-21 21:12:13 +0600

    Product routes created

---
+++ server/routes/productRoutes.js
@@ -0,0 +1,36 @@
+import express from "express";
+import Product from "../models/productModel.js";
+import asyncHandler from "express-async-handler";
+
+const router = express.Router();
+
+// @Description Fetch all products
+// @routes GET/api/products
+// @access public
+
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+
+    res.json(products);
+  })
+);
+
+// @Description Fetch single products
+// @routes GET/api/products/:id
+// @access public
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  })
+);
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-import products from "./data/product.js";
 import connectDB from "./config/db.js";
 import "colors";
+import productRoutes from "./routes/productRoutes.js";
 
 const PORT = process.env.PORT || 5000;
 
@@ -16,14 +16,7 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 app.listen(
   PORT,
