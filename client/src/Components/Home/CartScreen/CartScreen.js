214954935502a8577720eeb4a6be3eabc6479899
Shakib448 <shakiba448@gmail.com>
2020-12-28 16:52:54 +0600

    Shopping cart added

--- client/src/Components/Home/CartScreen/CartScreen.js
+++ client/src/Components/Home/CartScreen/CartScreen.js
@@ -5,13 +5,14 @@ import {
   Col,
   ListGroup,
   Image,
-  From,
+  Form,
   Button,
-  Cart,
+  Card,
 } from "react-bootstrap";
 
-import { useParams, useLocation, useHistory } from "react-router-dom";
+import { useParams, useLocation, useHistory, Link } from "react-router-dom";
 import { addToCart } from "../../../redux/actions/cartActions";
+import Message from "../../Message/Message";
 
 const CartScreen = () => {
   const { id } = useParams();
@@ -30,10 +31,106 @@ const CartScreen = () => {
     }
   }, [dispatch, id, qty]);
 
+  const removeFromCart = (id) => {
+    console.log(id);
+  };
+
+  const checkoutHandler = () => {
+    history.push("/login?redirect=shipping");
+  };
+
   return (
-    <div>
-      <h1>cart</h1>
-    </div>
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            Your cart is empty <Link to='/'>Go Back</Link>{" "}
+          </Message>
+        ) : (
+          <ListGroup variant='flush'>
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.id}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link
+                      style={{
+                        textDecoration: "none",
+                        color: "black",
+                        fontWeight: "bold",
+                      }}
+                      to={`/product/${item.id}`}
+                    >
+                      {item.name}
+                    </Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>
+                    <Form>
+                      <Form.Control
+                        as='select'
+                        value={item.qty}
+                        onChange={(e) =>
+                          dispatch(
+                            addToCart(item.product, Number(e.target.value))
+                          )
+                        }
+                      >
+                        {[...Array(item.countInStock).keys()].map((x) => (
+                          <option key={x + 1} value={x + 1}>
+                            {" "}
+                            {x + 1}{" "}
+                          </option>
+                        ))}
+                      </Form.Control>
+                    </Form>
+                  </Col>
+                  <Col md={2}>
+                    <Button
+                      type='button'
+                      variant='light'
+                      onClick={() => removeFromCart(item.product)}
+                    >
+                      <i className='fas fa-trash'></i> Remove
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h4>
+                {" "}
+                SubTotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                items $
+                {cartItems
+                  .reduce((acc, item) => acc + item.qty * item.price, 0)
+                  .toFixed(2)}
+              </h4>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                type='button'
+                className='btn-block'
+                disable={cartItems.length === 0}
+                onClick={checkoutHandler}
+                variant='dark'
+              >
+                Proceed To Checkout
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
   );
 };
 
