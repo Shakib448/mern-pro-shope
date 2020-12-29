45fe74575f7979c0b936d3542a5d6ad19a021ae8
Shakib448 <shakiba448@gmail.com>
2020-12-29 17:09:49 +0600

    Headers token added

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -2,14 +2,15 @@ import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
 import generateToken from "../utils/generateToken.js";
 
-// @Description Fetch all products
-// @routes GET/api/products
-// @access public
+// @Description Auth data
+// @routes Post/api/users/login
+// @access Public
 
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
+
   if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
@@ -24,4 +25,24 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+// @Description Get user profile
+// @routes GET/api/users/profile
+// @access Private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export { authUser, getUserProfile };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { authUser } from "../controllers/userControllers.js";
+import { authUser, getUserProfile } from "../controllers/userControllers.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/login", authUser);
+router.get("/profile", protect, getUserProfile);
 
 export default router;
