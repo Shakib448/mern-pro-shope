e14b37f7e4046a6469d82ac371abf3cb4a94141c
Shakib448 <shakiba448@gmail.com>
2020-12-29 16:10:32 +0600

    error resolved

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,4 +1,5 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
 
 // @Description Fetch all products
 // @routes GET/api/products
@@ -7,7 +8,19 @@ import asyncHandler from "express-async-handler";
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
-  res.send({ email, password });
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: null,
+    });
+  } else {
+    res.status(401);
+    throw new Error("INvalid email and password");
+  }
 });
 
 export { authUser };
