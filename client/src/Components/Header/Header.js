232b21e8aa34d73e0ce57be8798abf77eca4c916
Shakib448 <shakiba448@gmail.com>
2020-12-30 16:45:12 +0600

    Logout method added

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -1,8 +1,19 @@
 import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logOut } from "../../redux/actions/userActions";
 
 const Header = () => {
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    dispatch(logOut());
+  };
+
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg'>
@@ -17,10 +28,21 @@ const Header = () => {
                 {" "}
                 <i className='fas fa-shopping-cart'></i> Cart
               </Nav.Link>
-              <Nav.Link as={Link} to='/login'>
-                {" "}
-                <i className='fas fa-user'></i> Sign In
-              </Nav.Link>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id='username'>
+                  <NavDropdown.Item as={Link} to='/profile'>
+                    Profile
+                  </NavDropdown.Item>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <Nav.Link as={Link} to='/login'>
+                  {" "}
+                  <i className='fas fa-user'></i> Sign In
+                </Nav.Link>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
--- client/src/redux/actions/userActions.js
+++ client/src/redux/actions/userActions.js
@@ -2,6 +2,7 @@ import {
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
 } from "../constants/userConstants";
 import axios from "axios";
 
@@ -31,4 +32,9 @@ export const login = (email, password) => async (dispatch) => {
           : error.message,
     });
   }
+};
+
+export const logOut = () => (dispatch) => {
+  localStorage.removeItem("userInfo");
+  dispatch({ type: USER_LOGOUT });
 };
