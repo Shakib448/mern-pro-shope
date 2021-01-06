a33ef8169308c9ab06f820f54e7a5aa64184e2a2
Shakib448 <shakiba448@gmail.com>
2021-01-06 09:19:36 +0600

    Order get by id added

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { addOrderItems } from "../controllers/orderControllers.js";
+import {
+  addOrderItems,
+  getOrderById,
+} from "../controllers/orderControllers.js";
 
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", protect, addOrderItems);
+router.get("/:id", protect, getOrderById);
 
 export default router;
