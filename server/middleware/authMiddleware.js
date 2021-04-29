c4de7051ad43657a1b709cc0926c20647951945d
Shakiba448 <shakiba448@gmail.com>
2021-04-30 01:24:06 +0600

    Mern Pro Shop

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,6 +1,8 @@
 import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 import asyncHandler from "express-async-handler";
+import dotenv from "dotenv";
+dotenv.config();
 
 const protect = asyncHandler(async (req, res, next) => {
   let token;
