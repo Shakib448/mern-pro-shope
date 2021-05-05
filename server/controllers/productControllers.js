cd68f22cb285fa8b288891be91518184b0ff5350
Shakiba448 <shakiba448@gmail.com>
2021-05-05 22:21:25 +0600

    Updating site

--- server/controllers/productControllers.js
+++ server/controllers/productControllers.js
@@ -7,7 +7,6 @@ import Product from "../models/productModel.js";
 
 export const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
-
   res.json(products);
 });
 
@@ -37,6 +36,48 @@ export const deleteProduct = asyncHandler(async (req, res) => {
     const removeProduct = await product.remove();
     res.json({ message: "Product removed" });
     return removeProduct;
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+// @Description Create Product
+// @routes Post/api/products
+// @access Private/Admin
+
+export const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "sample name",
+    price: 0,
+    user: req.user._id,
+    image: "https://i.ibb.co/q9QZnkL/airpods.jpg",
+    category: "sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "sample description",
+  });
+  const createProduct = await product.save();
+  res.status(201).json(createProduct);
+});
+
+// @Description Update Product
+// @routes Put/api/products/:id
+// @access Private/Admin
+
+export const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, category, countInStock } = req.body;
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.countInStock = countInStock;
+    product.category = category;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
   } else {
     res.status(404);
     throw new Error("Product not found");
