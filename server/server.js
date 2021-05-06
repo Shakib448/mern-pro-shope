cf0ab2d7367a0754d720305731a2242e4725c797
Shakiba448 <shakiba448@gmail.com>
2021-05-06 17:24:20 +0600

    Updating site

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ import "colors";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import path from "path";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 const PORT = process.env.PORT || 5000;
@@ -26,10 +28,15 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
 
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
+// File
+const __dirname = path.resolve();
+app.use("/upload", express.static(path.join(__dirname, "/uploads")));
 
 // Not found api
 app.use(notFound);
