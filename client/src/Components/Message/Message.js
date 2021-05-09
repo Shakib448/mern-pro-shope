7a2a84466d6afb586ddc1207d9643471469f4db0
Shakiba448 <shakiba448@gmail.com>
2021-05-09 16:25:07 +0600

    Updating Site

--- client/src/Components/Message/Message.js
+++ client/src/Components/Message/Message.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Alert } from "react-bootstrap";
 
 const Message = ({ variant, children }) => {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    if (!children) {
+      setVisible(false);
+      return;
+    }
+    setVisible(true);
+    const timer = setTimeout(() => {
+      setVisible(false);
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, [children]);
+
+  if (!visible) return null;
+
   return <Alert variant={variant}>{children}</Alert>;
 };
 
