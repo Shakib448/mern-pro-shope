1b4aea54dc48b2f9564d49fed858c3c541378b54
Shakiba448 <shakiba448@gmail.com>
2021-05-09 16:40:18 +0600

    Deploying heroku

--- server/server.js
+++ server/server.js
@@ -43,17 +43,9 @@ app.get("/api/config/paypal", (req, res) =>
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running....");
-  });
-}
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
 
 // Not found api
 app.use(notFound);
